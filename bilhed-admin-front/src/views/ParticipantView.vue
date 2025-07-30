d16cf2a7f3f0afebb44ad32d658102d582a51d5d
Girgil <giraud.nicolas36@gmail.com>
2025-07-30 11:07:00 +0200

    [Admin][Front] fix incorrect draw order

--- bilhed-admin-front/src/views/ParticipantView.vue
+++ bilhed-admin-front/src/views/ParticipantView.vue
@@ -42,7 +42,7 @@
           <td>{{ g.group.drawOrder }}</td>
           <td><DateView :date="getLimitDate(g.group, member.id)" format="DD/MM HH:mm" sup=""/></td>
           <td class="d-flex align-items-center justify-content-end">
-            <template v-if="g.group.groupPayment && g.group.referentId === member.id && g.group.drawOrder !== undefined">
+            <template v-if="typeof g.group.drawOrder === 'number' && (!g.group.groupPayment || g.group.groupPayment && g.group.referentId === member.id)">
               <button type="button" class="btn btn-link btn-sm text-dark" title="Notify success" @click="notifyOne(member.id, 'success')" :disabled="loading"><BiSendCheck/></button>
               <button type="button" class="btn btn-link btn-sm text-dark" title="Notify waiting" @click="notifyOne(member.id, 'waiting')" :disabled="loading"><BiSendExclamation/></button>
               <button type="button" class="btn btn-link btn-sm text-dark" title="Notify failed" @click="notifyOne(member.id, 'failed')" :disabled="loading"><BiSendX/></button>
@@ -58,7 +58,7 @@
     <nav class="navbar sticky-bottom bg-light">
       <div class="container-fluid">
         <div>
-          <template v-if="groups[0].group.drawOrder !== undefined">
+          <template v-if="oneGroupDrawn">
             <button type="button" class="btn btn-primary me-1" @click="notifySel('success')" :disabled="loading"><BiSendCheck/> Notify success</button>
             <button type="button" class="btn btn-warning me-1" @click="notifySel('waiting')" :disabled="loading"><BiSendExclamation/> Notify waiting</button>
             <button type="button" class="btn btn-outline-danger me-4" @click="notifySel('failed')" :disabled="loading"><BiSendX/> Notify failed</button>
@@ -119,6 +119,10 @@ export default defineComponent({
     },
     checked(): Person[] {
       return this.groups.flatMap(g => g.members.filter(m => m.checked))
+    },
+    oneGroupDrawn() {
+      // true if at least one group has a draw order
+      return this.groups.filter(g => typeof g.group.drawOrder === 'number').length > 0
     }
   },
 
